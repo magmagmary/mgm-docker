5b8ed95623571e9dd928682591b928d0b199bc6e
Maryam <maryam.esmaeilzadeh@erasys.de>
2026-04-04 19:20:24 +0200

    refactor: update MongoDB user credentials and enhance Docker startup script for key-value database

--- key-value-db/db-config/mongo-init.js
+++ key-value-db/db-config/mongo-init.js
@@ -1,12 +1,12 @@
 const DATABASE_NAME = process.env.DATABASE_NAME;
-const MONGO_ROOT_USERNAME = process.env.MONGO_ROOT_USERNAME;
-const MONGO_ROOT_PASSWORD = process.env.MONGO_ROOT_PASSWORD;
+const MONGO_USERNAME = process.env.MONGO_KEY_VALUE_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_KEY_VALUE_PASSWORD;
 
 db  = db.getSiblingDB(DATABASE_NAME);
 
 db.createUser({
-  user: MONGO_ROOT_USERNAME,
-  pwd: MONGO_ROOT_PASSWORD,
+  user: MONGO_USERNAME,
+  pwd: MONGO_PASSWORD,
   roles: [
     { role: "readWrite", db: DATABASE_NAME },
   ],
