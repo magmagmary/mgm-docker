066dd871e9c68bff851d6739235d4ed24cc31288
Maryam <maryam.esmaeilzadeh@erasys.de>
2026-04-05 11:06:00 +0200

    feat: cleanup script

---
+++ key-value-db/scripts/cleanup-db.sh
@@ -0,0 +1,26 @@
+# Sources
+SCRIPT_DIR=$(dirname "$0")
+source "$SCRIPT_DIR/../.env.volume"
+source "$SCRIPT_DIR/../.env.network"
+source "$SCRIPT_DIR/../.env.db"
+
+if [ "$(docker ps -q -f name=$DB_CONTAINER_NAME)" ]; then
+    docker stop $DB_CONTAINER_NAME
+    echo "Container $DB_CONTAINER_NAME stopped and removed"
+else
+    echo "Container $DB_CONTAINER_NAME not found"
+fi
+
+if [ "$(docker volume ls -q -f name=$VOLUME_NAME)" ]; then
+    docker volume rm $VOLUME_NAME
+    echo "Volume $VOLUME_NAME removed"
+else
+    echo "Volume $VOLUME_NAME not found"
+fi
+
+if [ "$(docker network ls -q -f name=$NETWORK_NAME)" ]; then
+    docker network rm $NETWORK_NAME
+    echo "Network $NETWORK_NAME removed"
+else
+    echo "Network $NETWORK_NAME not found"
+fi
--- key-value-db/scripts/start-db.sh
+++ key-value-db/scripts/start-db.sh
@@ -1,6 +1,11 @@
+# Sources
+SCRIPT_DIR=$(dirname "$0")
+source "$SCRIPT_DIR/../.env.volume"
+source "$SCRIPT_DIR/../.env.network"
+source "$SCRIPT_DIR/../.env.db"
+
 MONGO_IMAGE="mongodb/mongodb-community-server"
 MONGO_VERSION="8.0.20-ubuntu2204"
-CONTAINER_NAME="mongodb"
 
 #ROOT CREDENTIALS
 MONGO_ROOT_USERNAME="admin"
@@ -11,20 +16,16 @@ DATABASE_NAME="key-value-db"
 MONGO_KEY_VALUE_USERNAME="key-value-user"
 MONGO_KEY_VALUE_PASSWORD="key-value-password"
 
-# Sources
-SCRIPT_DIR=$(dirname "$0")
-source "$SCRIPT_DIR/../.env.volume"
-source "$SCRIPT_DIR/../.env.network"
 
-if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-    echo "Container $CONTAINER_NAME already exists"
+if [ "$(docker ps -q -f name=$DB_CONTAINER_NAME)" ]; then
+    echo "Container $DB_CONTAINER_NAME already exists"
     exit 1;
 else
     source "$SCRIPT_DIR/setup.sh"
 fi
 
-docker run -d \
---name $CONTAINER_NAME \
+docker run -d --rm \
+--name $DB_CONTAINER_NAME \
 -e MONGO_INITDB_ROOT_USERNAME=$MONGO_ROOT_USERNAME \
 -e MONGO_INITDB_ROOT_PASSWORD=$MONGO_ROOT_PASSWORD \
 -e DATABASE_NAME=$DATABASE_NAME \
